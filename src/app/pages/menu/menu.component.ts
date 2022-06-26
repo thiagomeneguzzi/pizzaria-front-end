d642cc2ee266cbd03903adb9303ee1b53e43c958
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-06-25 21:06:34 -0300

    added other pizzas

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -9,7 +9,10 @@ import { Pizzas } from 'src/app/shared/interfaces/pizza';
 })
 export class MenuComponent implements OnInit {
 
-  pizzas: Pizzas = []
+  mostOrderedPizzas: Pizzas = []
+  traditionalPizzas: Pizzas = []
+  specialPizzas: Pizzas = []
+  premiumPizzas: Pizzas = []
 
   constructor() { }
 
@@ -19,16 +22,44 @@ export class MenuComponent implements OnInit {
 
   public getPizzas(numberOfPizzas: number) {
     for(let i = 0; i < numberOfPizzas; i++) {
-      this.pizzas.push(
+      this.mostOrderedPizzas.push(
         {
           id: 'jndsa',
-          name: 'Pizza Peperonni',
+          name: 'Peperonni',
           description: 'Mussarela, peperonni.',
           image: 'https://s.calendarr.com/upload/datas/pi/zz/pizza_c.jpg?auto_optimize=low&width=640',
           type: PizzaPrices.traditional
         }
       );
+      this.traditionalPizzas.push(
+        {
+          id: 'jndsaa',
+          name: 'Quatro queijos',
+          description: 'Mussarela, provolone, gorgonzola e prato.',
+          image: 'https://luccapizzaria.com.br/wp-content/uploads/2020/04/4-queijos.jpg',
+          type: PizzaPrices.traditional
+        }
+      );
+      this.specialPizzas.push(
+        {
+          id: 'jndsaa',
+          name: 'Bacon',
+          description: 'Mussarela, bacon.',
+          image: 'https://thumb-cdn.soluall.net/prod/shp_products/sp512box/5db0a0b8-a0bc-4bbe-b895-66acac1e00f4/5db0a0b8-e60c-41aa-ba12-66acac1e00f4.jpg',
+          type: PizzaPrices.special
+        }
+      );
+      this.premiumPizzas.push(
+        {
+          id: 'jndsaa',
+          name: 'Camarão',
+          description: 'Mussarela, camarão, cebola, cheddar e orégano.',
+          image: 'https://shoppoupeaqui.com.br/1732-large_default/pizza-camarao-c-cheddar.jpg',
+          type: PizzaPrices.premium
+        }
+      );
     }
   }
+
 
 }
--- src/app/shared/enums/pizza-prices.ts
+++ src/app/shared/enums/pizza-prices.ts
@@ -2,16 +2,16 @@ export const PizzaPrices = {
     traditional: {
         order: 1,
         name: 'Tradicional',
-        price: 39.9,
+        price: 39.90,
     },
     special: {
         order: 2,
         name: 'Especial',
-        price: 49.9,
+        price: 49.90,
     },
     premium: {
         order: 3,
         name: 'Premium',
-        price: 59.9,
+        price: 59.90,
     },
 } as const
